31bc5c1aae1d4b90c8340d434f23d249ecad504a
bugsounet <bugsounet@bugsnana.fr>
2020-06-17 23:02:35 +0200

    update installer

--- installer/preinstall.sh
+++ installer/preinstall.sh
@@ -25,9 +25,6 @@ source utils.sh
 # module name
 Installer_module="Assistant2Display"
 
-# use beep request questions ?
-Installer_beep=false
-
 # check version in package.json file
 Installer_version="$(cat ../package.json | grep version | cut -c14-30 2>/dev/null)"
 
@@ -41,20 +38,6 @@ if [ "$EUID" -eq 0 ]; then
   Installer_error "npm install must not be used as root"
   exit 1
 fi
-
-# Check platform compatibility
-Installer_info "Checking OS..."
-Installer_checkOS
-if  [ "$os_name" == "raspbian" ] && [ "$os_version" -lt 10 ]; then
-  Installer_error "OS Detected: $OSTYPE ($os_name $os_version $arch)"
-  Installer_error "Unfortunately, this program does not work with your OS"
-  Installer_error "Try compiling manually or updating to the lasted version"
-  exit 0
-else
-  Installer_success "OS Detected: $OSTYPE ($os_name $os_version $arch)"
-fi
-
-echo
 
 Installer_info "Installing all library..."
 echo
